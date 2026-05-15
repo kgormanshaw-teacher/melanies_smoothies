f9b182398e5a044b7001d189ab6b80fb99f5474a
kgormanshaw-teacher <kgormanshaw@otip.com>
2026-05-15 21:54:51 +0000

    comment

--- streamlit_app.py
+++ streamlit_app.py
@@ -68,8 +68,9 @@ st.write("The name on your smoothie wil be ", name_on_order)
 # st.write("Your favourite fruit is:", option)
 
 session = get_active_session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name')).sort(col('fruit_name'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name')).sort(col('fruit_name'),col('search_on'))
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop
 
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients",
