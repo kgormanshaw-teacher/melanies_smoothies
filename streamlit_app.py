08e14ac366dc56609608ce4847da37b639df1c02
kgormanshaw-teacher <kgormanshaw@otip.com>
2026-05-15 21:52:37 +0000

    fruit name

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import os
 import toml
 import streamlit as st
+import requests  
 from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 from snowflake.snowpark import Session
@@ -83,14 +84,12 @@ if ingredients_list:
     
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-
-    #st.write(ingredients_string)
-
+        st.subheader(fruit_chosen + ' Nutrition Information ')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)  
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+  
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
                     values ('""" + ingredients_string + """','"""+name_on_order+ """')"""
-
-    #st.write(my_insert_stmt)
-    # st.stop()
 
     time_to_insert = st.button('Submit Order')
     if time_to_insert:
@@ -98,6 +97,5 @@ if ingredients_list:
         st.success('Your Smoothie is ordered, '+name_on_order+'!', icon="✅")
 
 
-import requests  
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-st.text(smoothiefroot_response)
+
+#st.text(smoothiefroot_response.json())
